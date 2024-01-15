506867848e1725d9316a4c350f3f627dba54b694
chaeyeon <codusl100@naver.com>
2024-01-15 17:04:38 +0900

    :sparkles: #10 Feat: EmailAuth optimize code imports

--- src/main/java/kr/co/teacherforboss/domain/EmailAuth.java
+++ src/main/java/kr/co/teacherforboss/domain/EmailAuth.java
@@ -1,8 +1,18 @@
 package kr.co.teacherforboss.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import kr.co.teacherforboss.domain.enums.Purpose;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
