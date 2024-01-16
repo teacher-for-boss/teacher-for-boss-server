c875403544a70ff6e799341979518059257fc958
minjikim <inkmjj@sookmyung.ac.kr>
2024-01-17 02:42:07 +0900

    :recycle: #13 Refactor: BaseEntity Annotations

--- src/main/java/kr/co/teacherforboss/domain/common/BaseEntity.java
+++ src/main/java/kr/co/teacherforboss/domain/common/BaseEntity.java
@@ -1,5 +1,6 @@
 package kr.co.teacherforboss.domain.common;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,18 +12,26 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Getter
 public abstract class BaseEntity {
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Getter
     private Status status = Status.ACTIVE;
 
+    @Column(updatable = false, nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @CreatedDate
+    @Getter
     private LocalDateTime createdAt;
 
+    @Column(updatable = false, nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @LastModifiedDate
+    @Getter
     private LocalDateTime updatedAt;
 }
