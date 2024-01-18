e7da19850d8dd1cd32dbf69eac7ccf3645941ac4
Eunse Jang <senuej37@gmail.com>
2024-01-18 23:41:43 +0900

    ✨ #18 Feat: 로그인 Controller 추가

--- src/main/java/kr/co/teacherforboss/web/controller/AuthController.java
+++ src/main/java/kr/co/teacherforboss/web/controller/AuthController.java
@@ -2,10 +2,11 @@ package kr.co.teacherforboss.web.controller;
 
 import jakarta.validation.Valid;
 import kr.co.teacherforboss.apiPayload.ApiResponse;
+import kr.co.teacherforboss.config.jwt.JwtTokenProvider;
 import kr.co.teacherforboss.converter.AuthConverter;
 import kr.co.teacherforboss.domain.Member;
 import kr.co.teacherforboss.domain.EmailAuth;
-import kr.co.teacherforboss.service.AuthService.AuthCommandService;
+import kr.co.teacherforboss.service.authService.AuthCommandService;
 import kr.co.teacherforboss.web.dto.AuthRequestDTO;
 import kr.co.teacherforboss.web.dto.AuthResponseDTO;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthCommandService authCommandService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/signup")
     public ApiResponse<AuthResponseDTO.JoinResultDTO> join(@RequestBody @Valid AuthRequestDTO.JoinDTO request){
@@ -31,5 +33,12 @@ public class AuthController {
     public ApiResponse<AuthResponseDTO.SendCodeMailResultDTO> sendCodeMail(@RequestBody @Valid AuthRequestDTO.SendCodeMailDTO request) {
         EmailAuth emailAuth = authCommandService.sendCodeMail(request);
         return ApiResponse.onSuccess(AuthConverter.toSendCodeMailResultDTO(emailAuth));
+    }
+
+    @PostMapping("/login")
+    public ApiResponse<AuthResponseDTO.TokenResponseDTO> login(@RequestBody @Valid AuthRequestDTO.LoginDTO request) {
+        Member member = authCommandService.login(request);
+        AuthResponseDTO.TokenResponseDTO tokenResponseDTO = jwtTokenProvider.createTokenResponse(member.getEmail(), member.getRole());
+        return ApiResponse.onSuccess(tokenResponseDTO);
     }
 }
