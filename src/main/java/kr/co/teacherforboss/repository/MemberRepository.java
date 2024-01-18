f8b443835691ea9e34b27f75e0e81f2486e01cde
chaeyeon <codusl100@naver.com>
2024-01-19 00:59:20 +0900

    :recycle: #21 Refactor: import문 제거

--- src/main/java/kr/co/teacherforboss/repository/MemberRepository.java
+++ src/main/java/kr/co/teacherforboss/repository/MemberRepository.java
@@ -5,8 +5,6 @@ import kr.co.teacherforboss.domain.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmailAndStatus(String email, Status status);
