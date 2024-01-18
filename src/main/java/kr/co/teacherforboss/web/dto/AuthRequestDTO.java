958819daea7534f58e12da9c63018bd1ad04d2a3
minjikim <inkmjj@sookmyung.ac.kr>
2024-01-19 00:51:29 +0900

    🩹 #22 Fix: add missing import

--- src/main/java/kr/co/teacherforboss/web/dto/AuthRequestDTO.java
+++ src/main/java/kr/co/teacherforboss/web/dto/AuthRequestDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import kr.co.teacherforboss.validation.annotation.ExistEmailAuth;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
