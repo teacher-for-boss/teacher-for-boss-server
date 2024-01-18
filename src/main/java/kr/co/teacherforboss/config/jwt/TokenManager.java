8a3c2fe7dda80dcbb428cba2bf300f509e234365
Eunse Jang <senuej37@gmail.com>
2024-01-18 23:47:43 +0900

    ✨ #18 Feat: TokenManager

---
+++ src/main/java/kr/co/teacherforboss/config/jwt/TokenManager.java
@@ -0,0 +1,30 @@
+package kr.co.teacherforboss.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TokenManager {
+
+    private final RedissonClient redissonClient;
+    private static final String REFRESH_TOKEN_MAP = "RefreshTokenMap";
+    private static final String ACCESS_TOKEN_DENIED_MAP = "AccessTokenDeniedMap";
+
+    public void addRefreshToken(String key, String value) {
+        RMap<String, String> tokenMap = redissonClient.getMap(REFRESH_TOKEN_MAP);
+        tokenMap.put(key, value);
+    }
+
+    public String getRefreshToken(String key) {
+        RMap<String, String> tokenMap = redissonClient.getMap(REFRESH_TOKEN_MAP);
+        return tokenMap.get(key);
+    }
+
+    public boolean existBlackListAccessToken(String token) {
+        RMap<String, String> map = redissonClient.getMap(ACCESS_TOKEN_DENIED_MAP);
+        return map.containsKey(token);
+    }
+}
