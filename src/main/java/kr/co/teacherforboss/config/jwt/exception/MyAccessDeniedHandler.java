ed112be6c09679461e4e61a050ae16c1a6a57515
Eunse Jang <senuej37@gmail.com>
2024-01-20 23:27:27 +0900

    ✨ #26 Feat: JWT Custom 예외 처리

---
+++ src/main/java/kr/co/teacherforboss/config/jwt/exception/MyAccessDeniedHandler.java
@@ -0,0 +1,37 @@
+package kr.co.teacherforboss.config.jwt.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.co.teacherforboss.apiPayload.ApiResponse;
+import kr.co.teacherforboss.apiPayload.code.status.ErrorStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.info("[접근 권한 X] URL : [{}]", request.getRequestURL());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String code = ErrorStatus.ACCESS_DENIED.getCode();
+        String message = ErrorStatus.ACCESS_DENIED.getMessage();
+
+        ApiResponse<ErrorStatus> apiResponse = ApiResponse.onFailure(code, message, null);
+        String jsonResponse = objectMapper.writeValueAsString(apiResponse);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write(jsonResponse);
+    }
+}
