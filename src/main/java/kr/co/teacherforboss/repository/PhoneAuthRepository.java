6e89ae4d9e3a3edcf1810e00bb5aa73a67f72522
minjikim <inkmjj@sookmyung.ac.kr>
2024-01-24 02:31:46 +0900

    🩹 #30 Fix: Add missed @Repository

--- src/main/java/kr/co/teacherforboss/repository/PhoneAuthRepository.java
+++ src/main/java/kr/co/teacherforboss/repository/PhoneAuthRepository.java
@@ -3,7 +3,9 @@ package kr.co.teacherforboss.repository;
 import kr.co.teacherforboss.domain.PhoneAuth;
 import kr.co.teacherforboss.domain.enums.Purpose;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhoneAuthRepository extends JpaRepository<PhoneAuth, Long> {
     boolean existsByIdAndPhoneAndPurposeAndIsChecked(Long phoneAuthId, String phone, Purpose purpose, String isChecked);
 
