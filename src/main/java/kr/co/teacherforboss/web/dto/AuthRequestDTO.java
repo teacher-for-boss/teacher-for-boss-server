aa923f5b06fd448d02bd984920dcd24465dd5e0d
Eunse Jang <senuej37@gmail.com>
2024-03-08 14:25:10 +0900

    ♻️ #110 Refactor: DTO valid message 통일

--- src/main/java/kr/co/teacherforboss/web/dto/AuthRequestDTO.java
+++ src/main/java/kr/co/teacherforboss/web/dto/AuthRequestDTO.java
@@ -17,28 +17,28 @@ public class AuthRequestDTO {
     @Getter
     @Builder
     public static class JoinDTO{
-        @Email
-        @NotNull
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        @NotNull(message = "이메일은 필수 입력값입니다.")
         String email;
 
-        @NotNull
+        @NotNull(message = "이메일 인증 식별자는 필수 입력값입니다.")
         Long emailAuthId;
 
-        @NotNull
+        @NotNull(message = "전화번호 인증 식별자는 필수 입력값입니다.")
         Long phoneAuthId;
 
-        @NotNull
+        @NotNull(message = "비밀번호는 필수 입력값입니다.")
         @Size(min = 8, max = 20, message = "비밀번호를 8~20자 사이로 입력해주세요.")
         @Pattern(regexp="(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+{}|:<>?~,-]).{8,20}", message = "비밀번호는 숫자, 영어, 특수문자를 포함해서 8 ~ 20자리 이내로 입력해주세요.")
         String password;
 
-        @NotNull
+        @NotNull(message = "비밀번호 확인은 필수 입력값입니다.")
         String rePassword;
 
-        @NotNull
+        @NotNull(message = "사용자 이름은 필수 입력값입니다.")
         String name;
 
-        @NotNull
+        @NotNull(message = "전화번호는 필수 입력값입니다.")
         @Pattern(regexp = "010([2-9])\\d{7,8}", message = "전화번호는 10 ~ 11 자리의 숫자만 입력 가능합니다.")
         String phone;
 
@@ -62,7 +62,7 @@ public class AuthRequestDTO {
         @CheckTrueOrFalse
         String agreementSms;
 
-        @NotNull(message = "Email 수신 동의 여부를 입력해주세요.")
+        @NotNull(message = "이메일 수신 동의 여부를 입력해주세요.")
         @CheckTrueOrFalse
         String agreementEmail;
 
@@ -74,8 +74,8 @@ public class AuthRequestDTO {
     @Getter
     @Builder
     public static class SendCodeMailDTO {
-        @NotNull(message = "email 값이 없습니다.")
-        @Email(message = "email 값이 이메일 형식이 아닙니다.")
+        @NotNull(message = "이메일은 필수 입력값입니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email;
 
         @CheckPurpose
@@ -85,10 +85,10 @@ public class AuthRequestDTO {
     @Getter
     @Builder
     public static class CheckCodeMailDTO {
-        @NotNull(message = "emailAuthId 값이 없습니다.")
+        @NotNull(message = "이메일 인증 식별자는 필수 입력값입니다.")
         Long emailAuthId;
 
-        @NotNull(message = "emailAuthCode 값이 없습니다.")
+        @NotNull(message = "이메일 인증 코드는 필수 입력값입니다.")
         @Pattern(regexp = "\\d{5}", message = "인증 코드는 5자리의 숫자로 이루어져 있어야 합니다.")
         String emailAuthCode;
     }
@@ -96,7 +96,7 @@ public class AuthRequestDTO {
     @Getter
     @Builder
     public static class SendCodePhoneDTO {
-        @NotNull(message = "phone 값이 없습니다.")
+        @NotNull(message = "전화번호는 필수 입력값입니다.")
         @Pattern(regexp = "010([2-9])\\d{7,8}", message = "전화번호는 10 ~ 11 자리의 숫자만 입력 가능합니다.")
         String phone;
 
@@ -110,10 +110,10 @@ public class AuthRequestDTO {
     @Getter
     @Builder
     public static class CheckCodePhoneDTO {
-        @NotNull(message = "phoneAuthId 값이 없습니다.")
+        @NotNull(message = "전화번호 인증 식별자는 필수 입력값입니다.")
         Long phoneAuthId;
 
-        @NotNull(message = "phoneAuthCode 값이 없습니다.")
+        @NotNull(message = "전화번호 인증 코드는 필수 입력값입니다.")
         @Pattern(regexp = "\\d{5}", message = "인증 코드는 4자리의 숫자로 이루어져 있어야 합니다.")
         String phoneAuthCode;
     }
@@ -122,7 +122,7 @@ public class AuthRequestDTO {
     @Jacksonized
     @Builder
     public static class FindEmailDTO {
-        @NotNull(message = "phoneAuthId 값이 없습니다.")
+        @NotNull(message = "전화번호 인증 식별자는 필수 입력값입니다.")
         Long phoneAuthId;
     }
 
@@ -130,30 +130,30 @@ public class AuthRequestDTO {
     @Jacksonized
     @Builder
     public static class FindPasswordDTO {
-        @NotNull(message = "emailAuthId 값이 없습니다.")
+        @NotNull(message = "이메일 인증 식별자는 필수 입력값입니다.")
         Long emailAuthId;
     }
 
     @Getter
     public static class LoginDTO {
-        @NotBlank
+        @NotBlank(message = "이메일 값은 필수 입력값입니다.")
         String email;
 
-        @NotBlank
+        @NotBlank(message = "비밀번호 값은 필수 입력값입니다.")
         String password;
     }
 
     @Getter
     public static class ResetPasswordDTO {
-        @NotNull(message = "memberId 값이 없습니다.")
+        @NotNull(message = "사용자 식별자 값은 필수 입력값입니다.")
         Long memberId;
 
-        @NotNull
+        @NotNull(message = "비밀번호는 필수 입력값입니다.")
         @Size(min = 8, max = 20, message = "비밀번호를 8~20자 사이로 입력해주세요.")
         @Pattern(regexp="(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+{}|:<>?~,-]).{8,20}", message = "비밀번호는 숫자, 영어, 특수문자를 포함해서 8 ~ 20자리 이내로 입력해주세요.")
         String password;
 
-        @NotNull
+        @NotNull(message = "비밀번호 확인은 필수 입력값입니다.")
         @Size(min = 8, max = 20, message = "비밀번호를 8~20자 사이로 입력해주세요.")
         String rePassword;
     }
@@ -162,14 +162,14 @@ public class AuthRequestDTO {
     @Builder
     public static class SocialLoginDTO {
 
-        @Email(message = "이메일 형식이 아닙니다.")
-        @NotNull(message = "이메일이 없습니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        @NotNull(message = "이메일은 필수 입력값입니다.")
         String email;
 
-        @NotNull(message = "이름이 없습니다.")
+        @NotNull(message = "이름은 필수 입력값입니다.")
         String name;
 
-        @NotNull(message = "전화번호가 없습니다.")
+        @NotNull(message = "전화번호는 필수 입력값입니다.")
         @Pattern(regexp = "010([2-9])\\d{7,8}", message = "전화번호는 10 ~ 11 자리의 숫자만 입력 가능합니다.")
         String phone;
 
--- src/main/java/kr/co/teacherforboss/web/dto/ExamRequestDTO.java
+++ src/main/java/kr/co/teacherforboss/web/dto/ExamRequestDTO.java
@@ -17,7 +17,7 @@ public class ExamRequestDTO {
     @Builder
     public static class TakeExamsDTO{
 
-        @NotNull
+        @NotNull(message = "시험지 답 리스트는 필수 입력값입니다.")
         List<TakeExamsChoicesDTO> questionAnsList;
 
     }
@@ -25,10 +25,10 @@ public class ExamRequestDTO {
     @Getter
     @Builder
     public static class TakeExamsChoicesDTO{
-        @NotNull
+        @NotNull(message = "시험 문항 식별자는 필수 입력값입니다.")
         Long questionId;
 
-        @NotNull
+        @NotNull(message = "시험 문항 선택번호 식별자는 필수 입력값입니다.")
         Long questionChoiceId;
     }
 }
