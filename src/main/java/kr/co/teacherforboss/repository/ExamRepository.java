1a6a00b132df9bdcdcb6fcd5a952278ee1976b25
minjikim <inkmjj@sookmyung.ac.kr>
2024-03-25 00:19:30 +0900

    🐞 #134 Fix: 시험 치룬 횟수 조회 쿼리 오류 수정

--- src/main/java/kr/co/teacherforboss/repository/ExamRepository.java
+++ src/main/java/kr/co/teacherforboss/repository/ExamRepository.java
@@ -6,6 +6,7 @@ import kr.co.teacherforboss.domain.Exam;
 import kr.co.teacherforboss.domain.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,8 +14,10 @@ public interface ExamRepository extends JpaRepository<Exam, Long> {
     Optional<Exam> findByIdAndStatus(Long examId, Status status);
     boolean existsByIdAndStatus(Long id, Status status);
 
-    @Query(value = "select distinct e "
-            + "from Exam e, MemberExam me "
-            + "where e = me.exam")
-    List<Exam> findAllTakenExamByMemberId(Long memberId);
+    @Query(value = "select distinct e.* "
+            + "from exam e, member_exam me "
+            + "where e.id = me.exam_id "
+            + "and me.member_id = :memberId "
+            + "and me.status = 'ACTIVE'", nativeQuery = true)
+    List<Exam> findAllTakenExamByMemberId(@Param("memberId") Long memberId);
 }
