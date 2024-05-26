d650b53c0b3fa16a8df096111b9a9e720e20771b
chaeyeon <codusl100@naver.com>
2024-05-27 00:51:23 +0900

    :sparkles: #158 Feat: BooleanType 타입 체크 메서드 추가

--- src/main/java/kr/co/teacherforboss/domain/enums/BooleanType.java
+++ src/main/java/kr/co/teacherforboss/domain/enums/BooleanType.java
@@ -1,5 +1,17 @@
 package kr.co.teacherforboss.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum BooleanType {
-    T, F
+    T(true),
+    F(false);
+
+    private final Boolean identifier;
+
+    public static Boolean of(BooleanType type) {
+        return type.equals(T);
+    }
 }
