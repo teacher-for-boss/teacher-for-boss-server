3db2b6d3fa55583b62bce59bb2dcaccad78fdab2
minjikim <inkmjj@sookmyung.ac.kr>
2024-05-29 17:32:17 +0900

    ♻️ #157 Refactor: 불필요한 Import 삭제

--- src/main/java/kr/co/teacherforboss/service/boardService/BoardQueryService.java
+++ src/main/java/kr/co/teacherforboss/service/boardService/BoardQueryService.java
@@ -1,9 +1,6 @@
 package kr.co.teacherforboss.service.boardService;
 
-import kr.co.teacherforboss.domain.Post;
 import kr.co.teacherforboss.web.dto.BoardResponseDTO;
-
-import java.util.List;
 
 public interface BoardQueryService {
     BoardResponseDTO.GetPostDTO getPost(Long postId);
