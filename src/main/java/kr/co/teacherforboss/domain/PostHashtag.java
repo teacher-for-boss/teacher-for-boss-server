a19d2d66fd0ec574837fde9788058cf54e03260d
Eunse Jang <senuej37@gmail.com>
2024-05-30 16:41:31 +0900

    ✨ #159 Feat: PostHashtag의 equals 메서드 커스텀

--- src/main/java/kr/co/teacherforboss/domain/PostHashtag.java
+++ src/main/java/kr/co/teacherforboss/domain/PostHashtag.java
@@ -11,6 +11,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity
 @Getter
 @Builder
@@ -24,4 +26,17 @@ public class PostHashtag extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hashtagId")
     private Hashtag hashtag;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostHashtag that = (PostHashtag) o;
+        return Objects.equals(hashtag.getName(), that.hashtag.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hashtag);
+    }
 }
