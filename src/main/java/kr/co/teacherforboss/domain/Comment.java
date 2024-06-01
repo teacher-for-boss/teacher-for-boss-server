c92494d426d321e99977bbd6e26b8b941b9e097e
Eunse Jang <senuej37@gmail.com>
2024-06-01 19:22:28 +0900

    ✨ #160 Feat: 보스톡 댓글 작성 API

---
+++ src/main/java/kr/co/teacherforboss/domain/Comment.java
@@ -0,0 +1,48 @@
+package kr.co.teacherforboss.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.co.teacherforboss.domain.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.jetbrains.annotations.NotNull;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Comment extends BaseEntity {
+
+    @NotNull
+    private Long parentId;
+
+    @NotNull
+    @Column(length = 400)
+    private String content;
+
+    @NotNull
+    @Column
+    @ColumnDefault("0")
+    private Integer likeCount;
+
+    @NotNull
+    @Column
+    @ColumnDefault("0")
+    private Integer dislikeCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId")
+    private Member member;
+}
