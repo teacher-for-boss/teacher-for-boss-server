03fc95c3e65348c3e07af0e9f0302304a6a4caa1
Poby <smg981024@gmail.com>
2024-06-25 22:27:33 +0900

    ❤️‍🩹 #197 Fix: 쿼리문 수정
    status = 'ACTIVE' 추가

--- src/main/java/kr/co/teacherforboss/repository/QuestionRepository.java
+++ src/main/java/kr/co/teacherforboss/repository/QuestionRepository.java
@@ -19,37 +19,40 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
 	Integer countAllByCategoryIdAndStatus(Long categoryId, Status status);
 	@Query(value = """
 			SELECT * FROM question 
-			WHERE category_id = :categoryId
+			WHERE category_id = :categoryId and status = 'ACTIVE'
 			ORDER BY like_count DESC, created_at DESC
 	""", nativeQuery = true)
 	Slice<Question> findSliceByIdLessThanOrderByLikeCountDesc(Long categoryId, PageRequest pageRequest);
 	@Query(value = """
 			SELECT * FROM question 
-			WHERE category_id = :categoryId 
+			WHERE category_id = :categoryId and status = 'ACTIVE'
 			ORDER BY view_count DESC, created_at DESC
 	""", nativeQuery = true)
 	Slice<Question> findSliceByIdLessThanOrderByViewCountDesc(Long categoryId, PageRequest pageRequest);
 	@Query(value = """
 			SELECT * FROM question 
-			WHERE category_id = :categoryId 
+			WHERE category_id = :categoryId and status = 'ACTIVE'
 			ORDER BY created_at DESC
 	""", nativeQuery = true)
 	Slice<Question> findSliceByIdLessThanOrderByCreatedAtDesc(Long categoryId, PageRequest pageRequest);
 	@Query(value = """
             SELECT * FROM question
-            WHERE category_id = :categoryId AND (like_count <= (SELECT like_count FROM question WHERE id = :questionId) AND id != :questionId)
+            WHERE category_id = :categoryId AND status = 'ACTIVE' 
+            	AND (like_count <= (SELECT like_count FROM question WHERE id = :questionId) AND id != :questionId)
             ORDER BY like_count DESC, created_at DESC
     """, nativeQuery = true)
 	Slice<Question> findSliceByIdLessThanOrderByLikeCountDescWithLastQuestionId(Long categoryId, Long questionId, PageRequest pageRequest);
 	@Query(value = """
             SELECT * FROM question
-            WHERE category_id = :categoryId AND (view_count <= (SELECT view_count FROM question WHERE id = :questionId) AND id != :questionId)
+            WHERE category_id = :categoryId AND status = 'ACTIVE' 
+            	AND (view_count <= (SELECT view_count FROM question WHERE id = :questionId) AND id != :questionId)
             ORDER BY view_count DESC, created_at DESC
     """, nativeQuery = true)
 	Slice<Question> findSliceByIdLessThanOrderByViewCountDescWithLastQuestionId(Long categoryId, Long questionId, PageRequest pageRequest);
 	@Query(value = """
 			SELECT * FROM question
-			WHERE category_id = :categoryId AND created_at < (SELECT created_at FROM question WHERE id = :questionId)
+			WHERE category_id = :categoryId AND status = 'ACTIVE' 
+				AND created_at < (SELECT created_at FROM question WHERE id = :questionId)
 			ORDER BY created_at DESC
 	""", nativeQuery = true)
 	Slice<Question> findSliceByIdLessThanOrderByCreatedAtDescWithLastQuestionId(Long categoryId, Long questionId, PageRequest pageRequest);
