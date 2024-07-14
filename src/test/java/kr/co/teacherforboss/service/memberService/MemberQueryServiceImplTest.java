1810e9af67e443635cb0907f7a750853bfb53bad
chaeyeon <codusl100@naver.com>
2024-07-14 15:07:51 +0900

    :art: #264 Style: 불필요한 import문 제거

--- src/test/java/kr/co/teacherforboss/service/memberService/MemberQueryServiceImplTest.java
+++ src/test/java/kr/co/teacherforboss/service/memberService/MemberQueryServiceImplTest.java
@@ -25,8 +25,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
