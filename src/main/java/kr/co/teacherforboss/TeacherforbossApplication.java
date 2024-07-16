6de920f5f1db607c35d89c8a400e51e36defdfd4
minjikim <inkmjj@sookmyung.ac.kr>
2024-07-17 02:08:53 +0900

    ✨ #239 Feat: Redis Cache 설정

--- src/main/java/kr/co/teacherforboss/TeacherforbossApplication.java
+++ src/main/java/kr/co/teacherforboss/TeacherforbossApplication.java
@@ -2,10 +2,12 @@ package kr.co.teacherforboss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class TeacherforbossApplication {
 
 	public static void main(String[] args) {
