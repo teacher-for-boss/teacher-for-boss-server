64a7538aa4bac0421484f7b654c79b22f8350e3d
minjikim <inkmjj@sookmyung.ac.kr>
2024-07-17 02:12:18 +0900

    ♻️ #239 Refactor: 안쓰는 import, 함수 삭제 및 import 추가

--- src/main/java/kr/co/teacherforboss/repository/PostRepository.java
+++ src/main/java/kr/co/teacherforboss/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package kr.co.teacherforboss.repository;
 
+import java.util.List;
 import kr.co.teacherforboss.domain.Post;
 import kr.co.teacherforboss.domain.enums.Status;
 import org.springframework.data.domain.PageRequest;
@@ -44,7 +45,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             ORDER BY created_at DESC
     """, nativeQuery = true)
     Slice<Post> findSliceByIdLessThanOrderByCreatedAtDesc(Long postId, PageRequest pageRequest);
-    Long countAllByTitleLikeOrContentLikeAndStatus(String titleKeyword, String contentKeyword, Status status);
     Slice<Post> findSliceByTitleContainingOrContentContainingAndStatusOrderByCreatedAtDesc(String titleKeyword, String contentKeyword, Status status, PageRequest pageRequest);
     @Query(value = """
             SELECT * FROM post 
