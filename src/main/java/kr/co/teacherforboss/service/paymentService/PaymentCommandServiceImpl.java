0957813fb0a34a5c28809ebf0c342e9338375c6a
minjikim <inkmjj@sookmyung.ac.kr>
2024-07-24 23:51:32 +0900

    ♻️ #268 Refactor: 계좌 변경 NotNull 체크 수정

--- src/main/java/kr/co/teacherforboss/service/paymentService/PaymentCommandServiceImpl.java
+++ src/main/java/kr/co/teacherforboss/service/paymentService/PaymentCommandServiceImpl.java
@@ -25,13 +25,11 @@ public class PaymentCommandServiceImpl implements PaymentCommandService {
     public TeacherInfo editTeacherAccount(PaymentRequestDTO.EditTeacherAccountDTO request){
         Member member = authCommandService.getMember();
         if (member.getRole() != Role.TEACHER) throw new MemberHandler(ErrorStatus.MEMBER_ROLE_NOT_TEACHER);
-        if (request.getBank() == null && request.getAccountNumber() == null && request.getAccountHolder() == null)
-            throw new MemberHandler(ErrorStatus.MEMBER_ACCOUNT_INFO_EMPTY);
 
         TeacherInfo teacherInfo = teacherInfoRepository.findByMemberIdAndStatus(member.getId(), Status.ACTIVE)
                 .orElseThrow(() -> new MemberHandler(ErrorStatus.TEACHER_INFO_NOT_FOUND));
 
         teacherInfo.editTeacherAccount(request.getBank(), request.getAccountNumber(), request.getAccountHolder());
-        return teacherInfoRepository.save(teacherInfo);
+        return teacherInfo;
     }
 }
--- src/main/java/kr/co/teacherforboss/web/controller/PaymentController.java
+++ src/main/java/kr/co/teacherforboss/web/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package kr.co.teacherforboss.web.controller;
 
+import jakarta.validation.Valid;
 import kr.co.teacherforboss.apiPayload.ApiResponse;
 import kr.co.teacherforboss.converter.PaymentConverter;
 import kr.co.teacherforboss.domain.TeacherInfo;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +33,9 @@ public class PaymentController {
     }
 
     @PatchMapping("/accounts")
-    public ApiResponse<PaymentResponseDTO.EditTeacherAccountDTO> editTeacherAccount(PaymentRequestDTO.EditTeacherAccountDTO request) {
+    public ApiResponse<PaymentResponseDTO.EditTeacherAccountDTO> editTeacherAccount(@RequestBody @Valid PaymentRequestDTO.EditTeacherAccountDTO request) {
         TeacherInfo teacherInfo = paymentCommandService.editTeacherAccount(request);
         return ApiResponse.onSuccess(PaymentConverter.toEditTeacherAccountDTO(teacherInfo));
     }
+
 }
