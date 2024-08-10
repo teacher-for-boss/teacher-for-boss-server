73344902bd5d3fbad78d5c0cace0ba98bdd8e58e
Poby <smg981024@gmail.com>
2024-08-11 01:01:05 +0900

    ❤️‍🩹 #266 Fix: 안 쓰는 DTO 삭제

--- src/main/java/kr/co/teacherforboss/web/dto/MypageResponseDTO.java
+++ src/main/java/kr/co/teacherforboss/web/dto/MypageResponseDTO.java
@@ -10,31 +10,6 @@ import java.util.List;
 
 public class MypageResponseDTO {
 
-    @Getter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class GetMyQuestionsDTO {
-        boolean hasNext;
-        List<MypageResponseDTO.GetMyQuestionsDTO.QuestionInfo> questionList;
-
-        @Getter
-        @AllArgsConstructor
-        public static class QuestionInfo {
-            Long questionId;
-            String title;
-            String content;
-            Boolean solved;
-            String selectedTeacher;
-            Integer bookmarkCount;
-            Integer answerCount;
-            Integer likeCount;
-            Boolean liked;
-            Boolean bookmarked;
-            LocalDateTime createdAt;
-        }
-    }
-    
     @Builder
     @Getter
     @NoArgsConstructor
