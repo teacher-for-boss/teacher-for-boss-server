eb43bc5822e6920403f94822698527cd9d124463
Poby <smg981024@gmail.com>
2024-08-11 01:02:26 +0900

    ❤️‍🩹 #266 Fix: import wildcard 수정

--- src/main/java/kr/co/teacherforboss/converter/BoardConverter.java
+++ src/main/java/kr/co/teacherforboss/converter/BoardConverter.java
@@ -1,7 +1,22 @@
 package kr.co.teacherforboss.converter;
 
 import kr.co.teacherforboss.config.S3Config;
-import kr.co.teacherforboss.domain.*;
+import kr.co.teacherforboss.domain.Answer;
+import kr.co.teacherforboss.domain.AnswerLike;
+import kr.co.teacherforboss.domain.Category;
+import kr.co.teacherforboss.domain.Comment;
+import kr.co.teacherforboss.domain.CommentLike;
+import kr.co.teacherforboss.domain.Hashtag;
+import kr.co.teacherforboss.domain.Member;
+import kr.co.teacherforboss.domain.Post;
+import kr.co.teacherforboss.domain.PostBookmark;
+import kr.co.teacherforboss.domain.PostHashtag;
+import kr.co.teacherforboss.domain.PostLike;
+import kr.co.teacherforboss.domain.Question;
+import kr.co.teacherforboss.domain.QuestionBookmark;
+import kr.co.teacherforboss.domain.QuestionHashtag;
+import kr.co.teacherforboss.domain.QuestionLike;
+import kr.co.teacherforboss.domain.TeacherInfo;
 import kr.co.teacherforboss.domain.enums.BooleanType;
 import kr.co.teacherforboss.domain.enums.ImageOrigin;
 import kr.co.teacherforboss.web.dto.BoardRequestDTO;
@@ -10,7 +25,12 @@ import kr.co.teacherforboss.web.dto.HomeResponseDTO;
 import kr.co.teacherforboss.web.dto.MypageResponseDTO;
 import org.springframework.data.domain.Slice;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class BoardConverter {
 
