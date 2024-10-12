d96648807dfb23635d3f5561e06cc616f42e7e0c
minjikim <inkmjj@sookmyung.ac.kr>
2024-10-12 10:52:13 +0900

    ❤️‍🩹 Fix: 카테고리 변경 누락 추가

--- src/main/java/kr/co/teacherforboss/domain/enums/QuestionCategory.java
+++ src/main/java/kr/co/teacherforboss/domain/enums/QuestionCategory.java
@@ -22,13 +22,13 @@ public enum QuestionCategory {
 
 	public static long getIdentifier(String name) {
 		switch (name) {
-			case "마케팅": return 1;
-			case "위생": return 2;
-			case "상권": return 3;
-			case "운영": return 4;
-			case "직원관리": return 5;
-			case "인테리어": return 6;
-			case "정책": return 7;
+			case "세무": return 1;
+			case "노무": return 2;
+			case "노하우": return 3;
+			case "마케팅": return 4;
+			case "위생": return 5;
+			case "상권": return 6;
+			case "인테리어": return 7;
 			default: return 0;
 		}
 
