b111d07dcbd55a5507c13a4a6b950333318183fd
minjikim <inkmjj@sookmyung.ac.kr>
2024-11-10 22:32:43 +0900

    ⚙️ Chore: standby workflow 수정

--- src/main/java/kr/co/teacherforboss/TeacherforbossApplication.java
+++ src/main/java/kr/co/teacherforboss/TeacherforbossApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
